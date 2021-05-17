c32eb420cc300a7fb0283149f062ffcd01775639
ReedS <ReedStock1992@gmail.com>
2021-05-16 17:17:47 -0700

    Added more packages and framework for pages and components needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ChallengeDescription from './containers/ChallengeDescription';
+import FolderBrowser from './components/FolderBrowser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Challenge Description</Link>
+            </li>
+            <li>
+              <Link to="/data">Data</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/data">
+            <FolderBrowser />
+          </Route>
+          <Route path="/">
+            <ChallengeDescription />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/DisplayData.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FolderBrowser from "../components/FolderBrowser";
+
+export default function DisplayData(){
+    return(
+        <FolderBrowser
+        
+        />
+    )
+}
